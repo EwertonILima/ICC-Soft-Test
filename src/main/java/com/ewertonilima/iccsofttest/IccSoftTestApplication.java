aabb966ec21a7bf3e5eee7ae5600d0c993afd442
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-10 14:58:37 -0300

    Project created

---
+++ src/main/java/com/ewertonilima/iccsofttest/IccSoftTestApplication.java
@@ -0,0 +1,13 @@
+package com.ewertonilima.iccsofttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IccSoftTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IccSoftTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewertonilima/iccsofttest/IccSoftTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewertonilima.iccsofttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IccSoftTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
