89d0a369931f634f4eac10e9563395837124bb28
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-10 18:26:19 -0300

    Stock Controller

---
+++ src/main/java/com/ewertonilima/iccsofttest/controllers/StockController.java
@@ -0,0 +1,36 @@
+package com.ewertonilima.iccsofttest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ewertonilima.iccsofttest.entities.Stock;
+import com.ewertonilima.iccsofttest.services.StockService;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/stocks")
+public class StockController {
+	
+	@Autowired
+	StockService stockService;
+	
+	@GetMapping
+	public ResponseEntity<List<Stock>> findAll() {
+		List<Stock> list = stockService.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping("/name/{name}")
+	public ResponseEntity<Stock> findByName(@PathVariable String name) {
+		Stock stock = stockService.findByName(name);
+		return ResponseEntity.ok().body(stock);
+	}
+
+}
