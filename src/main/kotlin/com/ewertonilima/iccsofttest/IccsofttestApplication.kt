6bd97cd7b531ae097ab9e9cf622b19b1f65746bc
ewertonilima <ewertoninaciolima@gmail.com>
2024-03-17 06:27:39 -0300

    🎉 Initial Spring WebFlux project

---
+++ src/main/kotlin/com/ewertonilima/iccsofttest/IccsofttestApplication.kt
@@ -0,0 +1,11 @@
+package com.ewertonilima.iccsofttest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IccsofttestApplication
+
+fun main(args: Array<String>) {
+	runApplication<IccsofttestApplication>(*args)
+}
